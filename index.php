2ab489dbbb7e2f321a98c9a608d34b49d6b83a93
IdaKThomsen <idak0001@student.eadania.dk>
2019-01-03 15:13:01 +0100

    alt okay!

--- index.php
+++ index.php
@@ -109,7 +109,10 @@
 
                 #quote{
                     grid-area: quote;
-                    display: block;   
+                    display: block;
+                    align-self: end;
+                    border-bottom: solid #e589ba 1px;
+                    
                  }
                 
                 #quoteimg{
@@ -144,8 +147,6 @@
     
     <div id="opgavenisigselv">
         <h1>Portfolio</h1>
-        <p> Arbejdsproces:</p>
-
         <p>Indhold:</p>
         <p>
             Vi var en gruppe på omkring 5 som aftalte at lave en studiegruppe, hvor vi kunne hjælpe hinanden med notater til de forskellige emner. Vi startede allerede i denne gruppe omkring en halvanden uge inden vi officielt fik opgaven.  Det har været en stor hjælp at sidde sammen og dele de forskellige noter /viden vi hver især havde, på den måde har jeg fået gennemgået stoffet  igen, hvilket har givet en bedre forståelse i sidste ende. Vi snakkede i fællesskab om hvad vi ville have med på siden, og hvordan det skulle stilles op.
@@ -159,25 +160,18 @@
         <p> 
             Hele designprocessen startede for mig, da jeg begyndte at lave wireframes. Jeg udarbejde først mobil versionen,  og der havde jeg en klar tanke om at den bare skulle være 1 kolonne, da jeg ellers syntes at det kan blive for småt, eller for presset. Da jeg kom til tablet versionen lavede jeg først en wireframe over at den skulle fylde 4 kolonner, fordi de 2 yderste skulle være "blanke", samme tanke havde jeg omkring web, Men jeg fandt hurtigt ud af at jeg bare ville lave hhv. tablet 2 kolonner og web 3. 
             Jeg har valgt at lave det gennemgående tema i på min hjemmeside lyserødt, da det er en af mine absolutte yndlingsfarver, på den måde kommer der også lidt personlighed ind over, hvilket jeg også syntes er en god ide. 
-
-        </p>
-    
-        <p>Generelt: </p>
-        <p>
-            Jeg havde hele tiden i hovedet at det skulle være så simpelt som muligt, da jeg ikke "tør" at bevæge mig ud på alt for dybt vand, da jeg stadig er i en læringsproces hvor jeg gør mit bedste for at skabe en forståelse for det hele, hovedsageligt kodningsdelen. 
         </p>
     </div>
     
     <div id="reflektion">
         <h1> Reflektion </h1>
         <p> 
-            Overordnet syntes jeg at det er men ret fed følelse at sidde med, når man ser det færdige resultat af sin side, også taget i betragtning at der er et par "små" fejl.
-
-            Jeg har mindet mig selv om i løbet af denne proces, at det handler ikke om at komme ud med det flotteste resultat, eller at være den bedste, men det handler derimod om min lærings proces 
-
-            Og jeg har igennem hele projektet lært noget nyt hver dag. Det har ikke altid været lige nemt at forholde sig til, men det har været en spændende proces. 
-
-            Jeg ser frem til at jeg kan komme til at udvikle mine færdigheder.
+            Overordnet syntes jeg at det er en ret fed følelse at sidde med, når man ser det færdige resultat af sin side, også taget i betragtning at der er et par "små" fejl. Men nå man tænker på at for bare 3 måneder siden, startede jeg på den her uddannelse uden nogen form for erfaring inden for hverken Photoshop, Illustrator eller HTML/CSS. Og nu har jeg bevæget mig ud i at lave en hjemmeside fra bunden. 
+            Jeg har mindet mig selv om i løbet af denne proces, at det handler ikke om at komme ud med det flotteste resultat, eller at være den bedste, men det handler derimod om min lærings proces Og jeg har igennem hele projektet lært noget nyt hver dag. Det har ikke altid været lige nemt at forholde sig til, men det har været en spændende proces. Jeg ser frem til at jeg kan komme til at udvikle mine færdigheder.
+            Jeg havde hele tiden i hovedet at det skulle være så simpelt som muligt, da jeg ikke "tør" at bevæge mig ud på alt for dybt vand, da jeg stadig er i en læringsproces hvor jeg gør mit bedste for at skabe en forståelse for det hele,  og sørge for at jeg kan drage paralleller imellem de forskellige fag. 
+            <br>
+            Jeg syntes at jeg kom rigtigt godt fra start ved at arbejde på teksterne inden at jeg overhovedet gik i gang med at kode. Det der virkelig blev en udfordring for mig var at forstå det her med at lave filerne om til .PHP og  .inc og selve processen med Filezilla. Da jeg var kommet ret langt ville min side lige pludselig ikke vise CSS 
+            Det viste sig at være fordi at jeg havde lagt filerne fra computeren forkert over på Filezilla. Men Ved at slette alt som allerede lå på Filezilla og oploade igen, så virkede det. Så Jeg har lært at forstå hvordan det skal bruges nu, hvilket har gjort den sidste del af projektet meget nemmere.  
         </p>
     </div>
             
